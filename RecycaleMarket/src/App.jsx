f0e60b9573b9c68b4e6dfb0a54ff8fed68dd7124
Дмитрий Щербаков <shherbakov.dimon@gmail.com>
2024-02-06 00:48:44 +0500

    learn css in react

--- RecycaleMarket/src/App.jsx
+++ RecycaleMarket/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Product from "./Product.jsx";
 import Card from "./Card.jsx";
-
+import Button from "./Button.jsx";
 // function App() {
 //   return(
 //     <>
@@ -13,13 +13,21 @@ import Card from "./Card.jsx";
 //   );
 // }
 
+// function App() {
+//   return(
+//     <>
+//     <Card/>
+//     <Card/>
+//     <Card/>
+    
+//     </>
+//   );
+// }
+
 function App() {
   return(
     <>
-    <Card/>
-    <Card/>
-    <Card/>
-    
+      <Button/>
     </>
   );
 }
