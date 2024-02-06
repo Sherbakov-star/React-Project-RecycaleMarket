d906f0642b93442110a46beaf79e5265563eb47e
Дмитрий Щербаков <shherbakov.dimon@gmail.com>
2024-02-06 23:26:48 +0500

    learn click events in react

--- RecycaleMarket/src/App.jsx
+++ RecycaleMarket/src/App.jsx
@@ -6,6 +6,7 @@ import Button from "./Button.jsx";
 import User from "./User.jsx";
 import UserGreeting from "./UserGreeting.jsx";
 import ProductList from "./ProductList.jsx";
+import ProfilePicture from "./ProfilePicture.jsx";
 // function App() {
 //   return(
 //     <>
@@ -56,24 +57,40 @@ import ProductList from "./ProductList.jsx";
 //   );
 // }
 
+// function App() {
+
+//   const phones = [{id:1,name:"Iphone SE",color:"black"},
+//                         {id:2,name:"Iphone 7",color:"red"},
+//                         {id:3,name:"Xiaomi Redmi 12",color:"green"},
+//                         {id:4,name:"Honor X9a",color:"white"},
+//                         {id:5,name:"Honor 9a",color:"black"}]
+
+//   const watches = [{id:6,name:"Apple watch 3",color:"black"},
+//                         {id:7,name:"Apple watch 9",color:"red"},
+//                         {id:8,name:"Xiaomi redmi watch 3",color:"green"},
+//                         {id:4,name:"Honor band 6",color:"white"},
+//                         {id:5,name:"Honor band 3",color:"black"}]                      
+//   return(
+//     <>
+//       {phones.length > 0 && <ProductList items={phones} category="Смартфоны"/>}
+//       {watches.length > 0 && <ProductList items={watches} category="Смарт-часы"/>}
+      
+//     </>
+//   );
+// }
+
+// function App() {
+//   return(
+//     <>
+//       <Button/>
+//     </>
+//   );
+// }
+
 function App() {
-
-  const phones = [{id:1,name:"Iphone SE",color:"black"},
-                        {id:2,name:"Iphone 7",color:"red"},
-                        {id:3,name:"Xiaomi Redmi 12",color:"green"},
-                        {id:4,name:"Honor X9a",color:"white"},
-                        {id:5,name:"Honor 9a",color:"black"}]
-
-  const watches = [{id:6,name:"Apple watch 3",color:"black"},
-                        {id:7,name:"Apple watch 9",color:"red"},
-                        {id:8,name:"Xiaomi redmi watch 3",color:"green"},
-                        {id:4,name:"Honor band 6",color:"white"},
-                        {id:5,name:"Honor band 3",color:"black"}]                      
   return(
     <>
-      {phones.length > 0 && <ProductList items={phones} category="Смартфоны"/>}
-      {watches.length > 0 && <ProductList items={watches} category="Смарт-часы"/>}
-      
+      <ProfilePicture/>
     </>
   );
 }
--- RecycaleMarket/src/Button.jsx
+++ RecycaleMarket/src/Button.jsx
@@ -10,8 +10,26 @@ function Button(){
         cursor: "pointer",
     }
 
+    // const handleClick = () => console.log("click");
+
+    // const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+    // let count = 0;
+    // const handleClick = (name) => {
+    //     if(count < 3){
+    //         count++;
+    //         console.log(`${name} you clicked me ${count} time/s`)
+    //     }
+    //     else{
+    //         console.log(`${name} stop clicking me!`)
+    //     }
+    // };
+
+    const handleClick = (e) => e.target.textContent = "COOl";
     return(
-        <button style={styles}>Купить</button>
+        // <button onClick={() => handleClick2("Bro")} style={styles}>Купить</button>
+        // <button onClick={() => handleClick("Bro")} style={styles}>Купить</button>
+        <button onClick={(e) => handleClick(e)} style={styles}>Купить</button>
+
     );
 }
 
